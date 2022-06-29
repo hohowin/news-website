9921244dd6da18e189b47406a44ec4a4e8a86aea
Howin Ho <ho1ho@yahoo.com>
2022-06-29 11:56:55 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 5000
+
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
+// Templating Engine
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+// Parsing middleware
+// Parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false })); // Deprecated
+app.use(express.urlencoded({extended: true})); // New
+
+
+
+// Routes
+const newsRouter = require('./src/routes/news')
+
+app.use('/', newsRouter)
+app.use('/article', newsRouter)
+
+// Listen on port 5000
+app.listen(port, () => console.log(`Listening on port ${port}`))
